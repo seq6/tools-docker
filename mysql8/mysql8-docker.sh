d57cba6c0c8955d397fcb25a28934aa109729961
zhangji <zhuceyong180@163.com>
2019-08-23 14:02:55 +0800

    fix mysql8 docker boot file

--- mysql8/mysql8-docker.sh
+++ mysql8/mysql8-docker.sh
@@ -3,9 +3,10 @@
 docker run -d -p 3306:3306 \
   --restart always \
   --privileged=true \
-  --name zhangji-mysql8 \
+  --name mysql8-docker \
   -e MYSQL_USER="worker" \
   -e MYSQL_PASSWORD="123456" \
   -e MYSQL_ROOT_PASSWORD="123456" \
   -v="$(pwd)"/conf/my.cnf:/etc/my.cnf \
-  -v="$(pwd)"/data:/var/lib/mysql mysql:latest
+  -v="$(pwd)"/data:/var/lib/mysql \
+  mysql:8

aa752c4590d1daa465bf0260c29a17f91c3b375d
zhangji <zhuceyong180@163.com>
2019-09-08 12:38:28 +0800

    删除冗余文件

--- tools-images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker pull mysql:8
-docker pull mysql:5.7
-docker pull redis:5
-docker pull redis:4
-docker pull nginx:latest
-docker pull elasticsearch:7.3.0
-docker pull elasticsearch:6.8.2

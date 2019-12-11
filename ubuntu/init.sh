f562664c57c17db5c1b99771e0f69291395ae8f4
zhangji <zhuceyong180@163.com>
2019-12-11 14:41:20 +0800

    fix

--- ubuntu/init.sh
+++ ubuntu/init.sh
@@ -1,4 +1,6 @@
-docker run -ti -v ~/work:/work ubuntu:18.04 bash
+#docker run -ti -v ~/work:/work ubuntu:18.04 bash
+
+docker run -ti -v ~/work:/work --network tools-docker_app_net --ip 177.18.0.8 --name tool-ubuntu1804 ubuntu:18.04 bash
 
 #正确退出系统方式：
 #先按，ctrl+p
